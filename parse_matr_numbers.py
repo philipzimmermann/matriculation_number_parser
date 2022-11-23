ffe8435492d63ed8d009d1ef6615d0ea111c7263
Philip Zimmermann <philip.zimmermann@tum.de!>
2022-11-23 23:16:58 +0100

    now parsing all fields to strings, and added better colored output

--- parse_matr_numbers.py
+++ parse_matr_numbers.py
@@ -1,5 +1,6 @@
 import csv
 
+from termcolor import colored
 import pandas as pd
 
 
@@ -10,17 +11,18 @@ def find_student_and_give_point(evaluation_df, matricle_number, name):
         if len(matricle_number_to_compare) != 8 or not matricle_number_to_compare.isdigit():
             continue
         if matricle_number_to_compare == matricle_number:
-            print(f"Found match for: {row[0]} should be: {name}")
+            text_1 = f"{colored('Match:', 'green')} \t{row[0]} {row[1]}"
+            print(text_1, " " * max(55 - len(text_1), 0), f" == {name}")
             if row[5] == '-':
                 row[5] = 1
             else:
-                print("Points already set to: ", row[5])
+                print(colored(f"Points already set to: {row[5]}", 'yellow'))
             return
-    print(f"No match for: {name} matr number: {matricle_number}")
+    print(colored(f"No match for: {name} matr number: {matricle_number}"), 'red')
 
 
 def main():
-    attendance = pd.read_csv(ATTENDANCE_PATH, sep=',')
+    attendance = pd.read_csv(ATTENDANCE_PATH, sep=',', converters={i: str for i in range(100)})
     evaluation = pd.read_csv(EVALUATION_PATH, sep=',', converters={i: str for i in range(100)})
 
     # Iterate over attendance table
@@ -28,10 +30,12 @@ def main():
         name = row[1].strip()
         matricle_number = row[2].strip()
 
-        # If matriculation number not ok skip and print error message
+        # Make sure matricle number is ok
+        if len(matricle_number) == 7:
+            matricle_number = '0' + matricle_number
         if len(matricle_number) != 8 or not matricle_number.isdigit():
-            print(
-                f"Wrong matr number in attendance file please check: Name: {name} \tMatriculation Number: {matricle_number}")
+            print(colored('Wrong matr number in attendance file please check:', 'red'),
+                  f"Name: {name} \tMatriculation Number: {matricle_number}")
             continue
 
         find_student_and_give_point(evaluation, matricle_number, name)
@@ -43,7 +47,7 @@ def main():
 
 
 if __name__ == '__main__':
-    ATTENDANCE_PATH = "02_Lecture/Lecture 02 Attendance.csv"
-    EVALUATION_PATH = "02_Lecture/Overcoming Obst 950636118 (W2223) Bewertungen-20221108_1833-comma_separated.csv"
+    ATTENDANCE_PATH = "04_Lecture/Lecture 04 Attendance.csv"
+    EVALUATION_PATH = "04_Lecture/Overcoming Obst 950636118 (W2223) Bewertungen-20221123_2126-comma_separated.csv"
 
     main()
