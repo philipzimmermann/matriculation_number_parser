267ff1ee4f0fbc65cd33b02d9911d453c1bb64a9
Philip Zimmermann <philip.zimmermann@tum.de!>
2023-02-27 00:26:54 +0100

    some more input checks

--- parse_matr_numbers.py
+++ parse_matr_numbers.py
@@ -2,6 +2,7 @@ import csv
 from termcolor import colored
 import pandas as pd
 import sys
+import os.path
 
 def find_student_and_give_point(evaluation_df, matricle_number, name):
     # Iterate over evaluation file and look for matriculation number
@@ -20,7 +21,25 @@ def find_student_and_give_point(evaluation_df, matricle_number, name):
     print(colored(f"No match for: {name} matr number: {matricle_number}", 'red'))
 
 
-def main():
+if __name__ == '__main__':
+
+    # Check inputs
+    if len(sys.argv)!=3:
+    	print("Please give the file paths as arguments like this: \n'python parse_matr_numbers.py [path to attendance_file] [path to evaluation_file]'")
+    	sys.exit()
+    
+    ATTENDANCE_PATH = sys.argv[1]
+    EVALUATION_PATH = sys.argv[2]
+    
+    if not os.path.isfile(ATTENDANCE_PATH):
+    	print("No file at path: "+ ATTENDANCE_PATH)
+    	sys.exit()
+    	
+    if not os.path.isfile(EVALUATION_PATH):
+    	print("No file at path: "+ EVALUATION_PATH)
+    	sys.exit()
+
+    # Read csv tables
     attendance = pd.read_csv(ATTENDANCE_PATH, sep=',', converters={i: str for i in range(100)})
     evaluation = pd.read_csv(EVALUATION_PATH, sep=',', converters={i: str for i in range(100)})
 
@@ -31,7 +50,7 @@ def main():
 
         # Make sure matricle number is ok
         if len(matricle_number) == 7:
-            matricle_number = '0' + matricle_number
+            matricle_number = '0' + matricle_number # Try to fix with initial zero
         if len(matricle_number) != 8 or not matricle_number.isdigit():
             print(colored('Wrong matr number in attendance file please check:', 'red'),
                   f"Name: {name} \tMatriculation Number: {matricle_number}")
@@ -40,16 +59,6 @@ def main():
         find_student_and_give_point(evaluation, matricle_number, name)
     # Drop Rows with empty matriculation number
     evaluation = evaluation.drop(evaluation[evaluation["Matrikelnummer"] == ""].index)
-    # Write back to csv file
+    # Write result to new csv file
     evaluation.to_csv('output.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
     print("Done.")
-
-
-if __name__ == '__main__':
-    print(f"Arguments count: {len(sys.argv)}")
-    assert len(sys.argv)==3
-    
-    ATTENDANCE_PATH = sys.argv[1]
-    EVALUATION_PATH = sys.argv[2]
-
-    main()
